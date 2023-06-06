1dc104d87cb20294601f8e27bcc501fde1cb52f2
pranav2050 <97501797+pranavpeter2050@users.noreply.github.com>
2023-06-06 16:09:16 +0530

    Adding login and authentication: Input OTP screen.

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="pt-16">
     <h1 class="text-3xl-font-semibold mb-4">Enter your phone number</h1>
-    <form action="#" @submit.prevent="handleLogin">
+    <form v-if="!waitingOnVerification" action="#" @submit.prevent="handleLogin">
       <div class="overflow-hidden shadow sm:rounded-md max-w-sm mx-auto text-left">
         <div class="bg-white px-4 py-5 sm:p-6">
           <div>
@@ -23,17 +23,46 @@
         </div>
       </div>
     </form>
+    
+    <!-- OTP verifiction -->
+    <form v-else action="#" @submit.prevent="handleVerification">
+      <div class="overflow-hidden shadow sm:rounded-md max-w-sm mx-auto text-left">
+        <div class="bg-white px-4 py-5 sm:p-6">
+          <div>
+            <input
+              id="phone"
+              class="mt-1 block w-full px-3 py-2 rounded-md border border-gray-300 shadow-sm"
+              name="phone"
+              placeholder="123456"
+              type="text"
+              v-maska data-maska="######"
+              v-model="credentials.login_code"
+            >
+          </div>
+        </div>
+        <div class="bg-gray-50 px-4 py-3 text-right sm:px-6">
+          <button type="submit" class="inline-flex justify-center rounded-md border border-transparent bg-black py-2 px-4 text-white">
+            Verify
+          </button>
+        </div>
+      </div>
+    </form>
   </div>  
 </template>
 
 <script setup>
 import { vMaska } from "maska"
-import { reactive } from "vue"
+import { reactive, ref } from "vue"
 import axios from "axios"
+import { useRouter } from "vue-router"
+
+const router = useRouter()
 
 const credentials = reactive({
-  phone: null
+  phone: null,
+  login_code: null
 })
+const waitingOnVerification = ref(false)
 
 const handleLogin = () => {
   axios.post("http://127.0.0.1:8000/api/login", {
@@ -41,11 +70,29 @@ const handleLogin = () => {
   })
     .then((respoonse) => {
       console.log(respoonse.data)
+      waitingOnVerification.value = true
     })
     .catch((error) => {
       console.error(error)
       alert(error.respoonse.data.message)
     })
+}
 
+const handleVerification = () => {
+  axios.post("http://127.0.0.1:8000/api/login/verify", {
+    phone: credentials.phone.replaceAll(" ", "").replace("+91-", ""),
+    login_code: credentials.login_code
+  })
+  .then((respoonse) => {
+    console.log(respoonse.data) // this should be an auth token
+    localStorage.setItem("token", respoonse.data)
+    router.push({
+      name: "index"
+    })
+  })
+  .catch((error) => {
+    console.error(error)
+    alert(error.respoonse.data.message)
+  })
 }
 </script>
