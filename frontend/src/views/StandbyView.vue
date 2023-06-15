019a123af78c12344445a418b9f43ec97b0d1837
pranav2050 <97501797+pranavpeter2050@users.noreply.github.com>
2023-06-15 10:36:06 +0530

    Driver flow: added loader in StandbyView
    Shows loader while we listen for a websocket event to look for a trip.

--- frontend/src/views/StandbyView.vue
+++ frontend/src/views/StandbyView.vue
@@ -1,6 +1,13 @@
 <template>
-  
+  <div class="pt-16">
+    <h1 class="text-3xl font-semibold mb-4">Waiting for ride request...</h1>
+    <div class="mt-8 flex justify-center">
+      <Loader/>
+    </div>
+  </div>
 </template>
 
 <script setup>
+import Loader from '@/components/Loader.vue';
+
 </script>
