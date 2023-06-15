cffe04b039d3c95e6fffcbd6f3ee331f9e99511d
pranav2050 <97501797+pranavpeter2050@users.noreply.github.com>
2023-06-15 10:57:40 +0530

    Changed the channel name for TripStarted event.
    As this channel should be visible to all drivers so that they can see when a Trip is available to pickup, we changed the name to a more generic one, example "drivers".

--- backend/app/Events/TripStarted.php
+++ backend/app/Events/TripStarted.php
@@ -36,7 +36,7 @@ class TripStarted
     public function broadcastOn(): array
     {
         return [
-            new Channel('passenger_' . $this->user->id),
+            new Channel('drivers'),
         ];
     }
 }
